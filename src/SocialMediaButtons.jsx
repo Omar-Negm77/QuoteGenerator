48736492b18103edf2f77c6aecca3053a915ad69
unknown <omar.negm77@gmail.com>
2023-11-09 23:33:59 -0800

    adding social icons

---
+++ src/SocialMediaButtons.jsx
@@ -0,0 +1,34 @@
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "react-share";
+
+import {
+  EmailIcon,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share";
+
+const SocialMediaButtons = ({ quote, url }) => {
+  return (
+    <>
+      <FacebookShareButton url={url} quote={quote} hashtag="">
+        <FacebookIcon size={36} round={true} />
+      </FacebookShareButton>
+      <EmailShareButton url={url} quote={quote} hashtag="">
+        <EmailIcon size={36} round={true} />
+      </EmailShareButton>
+      <TwitterShareButton url={url} quote={quote} hashtag="">
+        <TwitterIcon size={36} round={true} />
+      </TwitterShareButton>
+      <WhatsappShareButton url={url} quote={quote} hashtag="">
+        <WhatsappIcon size={36} round={true} />
+      </WhatsappShareButton>
+    </>
+  );
+};
+
+export default SocialMediaButtons;
